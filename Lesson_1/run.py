6fa0c7f905b0f843efa959ad0b39a8756a0e20d0
yarikmik <yarik.mik@gmail.com>
2022-01-22 23:23:03 +0700

    + вьюшки на 3 страницы

--- Lesson_1/run.py
+++ Lesson_1/run.py
@@ -6,6 +6,6 @@ from urls import routes, fronts
 
 application = Framework(routes, fronts)
 
-with make_server('', 8080, application) as httpd:
+with make_server('localhost', 8080, application) as httpd:
     print("Запуск на порту 8080...")
     httpd.serve_forever()
--- Lesson_1/views.py
+++ Lesson_1/views.py
@@ -6,6 +6,11 @@ class Index:
         return '200 OK', render('index.html', date=request.get('date', None))
 
 
+class Contact:
+    def __call__(self, request):
+        return '200 OK', render('contact.html', date=request.get('date', None))
+
+
 class About:
     def __call__(self, request):
-        return '200 OK', 'about'
+        return '200 OK', render('about.html', date=request.get('date', None))
