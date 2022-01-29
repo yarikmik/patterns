af54eca3da1c9b7ad05ac851872c4c7ff510b4cd
yarikmik <yarik.mik@gmail.com>
2022-01-29 16:40:31 +0700

    lesson_3 start

---
+++ Lesson_3/urls.py
@@ -0,0 +1,20 @@
+from datetime import date
+from views import Index, About, Contact
+
+
+# front controller
+def secret_front(request):
+    request['date'] = date.today().strftime("%B %d, %Y")
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/about/': About(),
+    '/contact/': Contact(),
+}
